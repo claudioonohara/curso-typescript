3e83cc873e9ec937f1b370016af3db9ec19866a7
claudio.onohara <claudioonohara@gmail.com>
2020-11-04 11:33:54 -0400

    Atribuindo Tipos Explícitos

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -12,3 +12,25 @@ console.log(idade);
 var possuiHobbies = false;
 //possuiHobbies = 1
 console.log(possuiHobbies);
+//tipo dianimico(declarando variavel sem inicializar e sem declarar tipo a torna dinamica, tipo any)
+var minhaIdade;
+minhaIdade = 26;
+console.log(typeof minhaIdade);
+minhaIdade = 'a idade é 26';
+console.log(typeof minhaIdade);
+//tipo explicito
+var minhaIdadeTipoExplicito;
+minhaIdadeTipoExplicito = 26;
+console.log(typeof minhaIdadeTipoExplicito);
+// minhaIdadeTipoExplicito = 'a idade é 26'
+// console.log(typeof minhaIdadeTipoExplicito)
+//string 
+var nomeExplicito = 'J';
+console.log(nomeExplicito);
+//numbers
+var idadeExplicito = 28;
+idadeExplicito = 28.5;
+console.log(idadeExplicito);
+//boolean
+var possuiHobbiesExplicito = false;
+console.log(possuiHobbiesExplicito);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -13,3 +13,30 @@ console.log(idade)
 let possuiHobbies = false
 //possuiHobbies = 1
 console.log(possuiHobbies)
+
+//tipo dianimico(declarando variavel sem inicializar e sem declarar tipo a torna dinamica, tipo any)
+let minhaIdade 
+minhaIdade = 26
+console.log(typeof minhaIdade)
+minhaIdade = 'a idade é 26'
+console.log(typeof minhaIdade)
+
+//tipo explicito
+let minhaIdadeTipoExplicito: number 
+minhaIdadeTipoExplicito = 26
+console.log(typeof minhaIdadeTipoExplicito)
+// minhaIdadeTipoExplicito = 'a idade é 26'
+// console.log(typeof minhaIdadeTipoExplicito)
+
+//string 
+let nomeExplicito: string = 'J'
+console.log(nomeExplicito)
+
+//numbers
+let idadeExplicito: number = 28
+idadeExplicito = 28.5
+console.log(idadeExplicito)
+
+//boolean
+let possuiHobbiesExplicito: boolean = false
+console.log(possuiHobbiesExplicito)
