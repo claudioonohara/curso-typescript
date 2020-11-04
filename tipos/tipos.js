ce43d0acacd43f9a9d188d58e2aff681f9275c86
claudio.onohara <claudioonohara@gmail.com>
2020-11-04 11:16:19 -0400

    tipos numericos e booleano

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -1,4 +1,14 @@
+"use strict";
 //string 
-let nome = 'J';
+var nome = 'J';
 console.log(nome);
-nome = 27
+// nome = 28
+//numbers
+var idade = 28;
+//idade = 'Ana'
+idade = 28.5;
+console.log(idade);
+//boolean
+var possuiHobbies = false;
+//possuiHobbies = 1
+console.log(possuiHobbies);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -2,3 +2,14 @@
 let nome = 'J'
 console.log(nome)
 // nome = 28
+
+//numbers
+let idade = 28
+//idade = 'Ana'
+idade = 28.5
+console.log(idade)
+
+//boolean
+let possuiHobbies = false
+//possuiHobbies = 1
+console.log(possuiHobbies)
